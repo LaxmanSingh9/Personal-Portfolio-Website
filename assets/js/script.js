e87c7f3b5316979f389473a616be3ad62f2a1a55
LaxmanSingh9 <laxmansisodiya77846@gmail.com>
2022-12-03 14:09:18 +0530

    Added resume btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("YgBP0VFMkcVk8uIdd");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_gz0xu4o', 'template_3pu1xq5', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Laxman Singh";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
